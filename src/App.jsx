1c30994a76c4736fcea369d011c3604ea5dae82d
Sai <saideepdasari30@gmail.com>
2025-01-13 15:01:03 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import { BrowserRouter , Routes , Route } from "react-router-dom";
+import {LoginPage} from "./routes/Routes"
+
+import "./App.css"
+
+function App(){
+  return(
+    <BrowserRouter>
+     <Routes>
+      <Route path="/login" element={<LoginPage/>} />
+     </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
